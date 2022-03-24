a85eef642b714b678a555e9977128b4cd90ea01c
iamLolade <adeniyiotemade@gmail.com>
2022-03-24 21:25:52 +0100

    Updated JS file | Styled individual bar

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let barContainer = document.getElementById("bars")
 
+const colors = ["#B1E26F", "#ED7C6E", "#FEFEFE", "#8DCCEF", "#F9D076"];
+
 const render = () => {
     let bar = "";
 
     for(let i = 0; i < 6; i++) {
         bar += `
-            <li class="bar" id=${i}>50%</li>
+            <li class="singleBar" style="background-color: ${colors[i]}" id=${i}>50%</li>
         `
     }
     barContainer.innerHTML = bar;
