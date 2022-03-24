c883513eeb7bd58199b0e01b747b1578e2802264
iamLolade <adeniyiotemade@gmail.com>
2022-03-24 23:10:35 +0100

    Updated HTML, CSS and JS files

--- index.js
+++ index.js
@@ -17,8 +17,26 @@ const render = () => {
 }
 
 finishBtn.addEventListener("click", () => {
-    const progress = document.querySelector(".singleBar");
-    progress.setAttribute("id", "play-animation")
+    const progress = document.querySelectorAll(".singleBar");
+    let i = 0;
+    if(i == 0) {
+        i = 1;
+        for(let j = 0; j < progress.length; j++) {
+            let elem = progress[j];
+            let width = 50;
+            let id = setInterval(frame, 10);
+            function frame() {
+                if(width >= 100) {
+                    clearInterval(id)
+                    i = 0;
+                } else {
+                    width++;
+                    elem.style.width = width + "%";
+                    elem.innerHTML = width + "%";
+                }
+            }
+        }
+    }
 })
 
 render();
